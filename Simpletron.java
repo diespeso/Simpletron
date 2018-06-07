07f36ea63d6b2e1163f1d678c2678b888e4738fa
Edgar <edgar.froot@gmail.com>
2018-06-07 13:00:12 -0700

    Update Simpletron.java

--- Simpletron.java
+++ Simpletron.java
@@ -16,11 +16,17 @@ class Simpletron {
     private static boolean modoDictado = false;
     private static int ordenPararInteractiva = 9999;
 
+    /**
+     * Creates a new Simpletron and turns it on calling prender() and sets the useMode using setModoDeUso().
+     **/
     Simpletron() {
         prender();
         setModoDeUso();
     }
 
+    /**
+     * Used when the modoDictado and the modoInteractivo are disabled.
+     **/
     void correr() {
         if(!modoInteractivo && !modoDictado) {
             procesador.procesarMemoria();
@@ -32,6 +38,11 @@ class Simpletron {
         }
     }
 
+    /**
+     * Loads the memory and makes a new Procesador to read and execute it.
+     *
+     * @param programa The set of instructions to be processed.
+     **/
     void cargarPrograma(int[] programa) {
         if(!modoInteractivo) {
             System.arraycopy(programa, 0, memoria, 0, programa.length);
@@ -39,6 +50,12 @@ class Simpletron {
         procesador = new Procesador(memoria);
     }
 
+    /**
+     * WHY IS THIS CALLED LIKE THIS? ISN'T THIS OBJECT ORIENTED PROGRAMMING???????
+     *
+     * Prints a prompt to the user, gets its input calling getEntrada() and  loops while the Simpletron is turned on.
+     * Processes every instruction on the memory and when it ends, turns off the Simpletron.
+     **/
     private void interpreteDeOrdenes() {
         System.out.print(">>> ");
         int orden = getEntrada();
@@ -53,10 +70,24 @@ class Simpletron {
         Simpletron.apagar();
     }
 
+    /**
+     * Starts the Dictado Mode for the Simpletron.
+     **/
     private void dictar() { procesador.dictar(); }
 
+    /**
+     * Gets the input from the user.
+     *
+     * @return The input from the user.
+     **/
     static int getEntrada() { return scanner.nextInt(); }
 
+    /**
+     * Gets the instruction that the user entered, but checks if it is valid first, this is done by
+     * calling isInstruccionValida() on the user's input.
+     *
+     * @return The revised instruction that the user entered.
+     **/
     static int getInstruccion() {
         int instruc = getEntrada();
         if(!isInstruccionValida(instruc)) {
@@ -66,15 +97,25 @@ class Simpletron {
         return instruc;
     }
 
-
+    /**
+     * Checks whether the instruction is valid or not.
+     *
+     * @return Whether the instruction is valid or not.
+     **/
     static boolean isInstruccionValida(int instruccion) {
         return Integer.toString(instruccion).length() == 4 || instruccion == 0;
     }
 
+    /**
+    * Turns on the simpletron and prints a message notifying the user.
+    **/
     private static void prender() {
         prendida = true;
         System.out.println("SIMPLETON ON");
     }
+    
+    /**
+     * 
     static void apagar() {
         System.out.println("SIMPLETON OFF");
         prendida = false;
